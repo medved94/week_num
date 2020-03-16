53d8988d72f19c83a644389567fda93b1ba61ba2
Medvedev Sergei <midved94@gmail.com>
2020-03-16 23:25:50 +0300

    adding gif and refresh

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 Date.prototype.getWeek = function () {
-    var onejan = new Date(this.getFullYear(), 0, 1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    let onejan = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()) / 7);
 }
-let weekNumber = (new Date()).getWeek();
-var now = new Date();
-document.getElementById("num").innerHTML = weekNumber
+
+window.onload = function () {
+    setTimeout('document.location.reload(true)', 86400000);
+    let weekNumber = (new Date()).getWeek();
+    //var now = new Date();
+    document.getElementById("num").innerHTML = weekNumber;
+
+}
+
+function show() {
+    let x = document.getElementById("snoop");
+    if (x.style.display != "block") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
